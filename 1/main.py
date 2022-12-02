fb3d6ac1c41a703f78bd6b4b128c7a47e982cb9a
msilva <msilva@synopsys.com>
2022-12-02 10:34:00 +0000

    convert py 2 to enum and format everything

--- 1/main.py
+++ 1/main.py
@@ -1,7 +1,7 @@
-f = open('1/input.txt')
+f = open("1/input.txt")
 elfs = [[]]
 for l in f.readlines():
-    if l != '\n':
+    if l != "\n":
         elfs[-1].append(int(l))
     else:
         elfs.append([])
@@ -13,5 +13,3 @@ print(max_elf)
 all_elfs_total = [sum(elf_food) for elf_food in elfs]
 all_elfs_total.sort(reverse=True)
 print(sum(all_elfs_total[:3]))
-
-
--- 2/main.py
+++ 2/main.py
@@ -1,25 +1,40 @@
-f = open('2/input.txt')
-
-ROCK = 'rock'
-PAPER = 'paper'
-SCISSORS = 'scissors'
-
-WIN = 'Z'
-DRAW = 'Y'
-LOSE = 'X'
+from enum import Enum
 
 
-def to_tool(char: str):
-    if char in ['A', 'X']:
-        return ROCK
-    if char in ['B', 'Y']:
-        return PAPER
-    if char in ['C', 'Z']:
-        return SCISSORS
+f = open("2/input.txt")
 
-nemesis_map = {ROCK: PAPER, PAPER: SCISSORS, SCISSORS: ROCK}
 
-def battle(p1, p2) -> int:
+class Tool(str, Enum):
+    ROCK = "rock"
+    PAPER = "paper"
+    SCISSORS = "scissors"
+
+    @staticmethod
+    def from_str(char: str):
+        if char in ["A", "X"]:
+            return Tool.ROCK
+        elif char in ["B", "Y"]:
+            return Tool.PAPER
+        elif char in ["C", "Z"]:
+            return Tool.SCISSORS
+        else:
+            raise ValueError("Invalid tool code")
+
+
+class Outcome(str, Enum):
+    WIN = "Z"
+    DRAW = "Y"
+    LOSE = "X"
+
+
+nemesis_map = {
+    Tool.ROCK: Tool.PAPER,
+    Tool.PAPER: Tool.SCISSORS,
+    Tool.SCISSORS: Tool.ROCK,
+}
+
+
+def battle(p1: Tool, p2: Tool) -> int:
     if p1 == p2:
         return 0
     if p1 == nemesis_map[p2]:
@@ -27,40 +42,41 @@ def battle(p1, p2) -> int:
     else:
         return 1
 
-def tool_score(tool):
-    if tool == ROCK:
+
+def tool_score(tool: Tool):
+    if tool == Tool.ROCK:
         return 1
-    elif tool == PAPER:
+    elif tool == Tool.PAPER:
         return 2
     else:
         return 3
 
 
-def play(opponent: str, me:str):
-    score = tool_score(me) + (battle(opponent, me)+1) * 3
+def play(opponent: Tool, me: Tool):
+    score = tool_score(me) + (battle(opponent, me) + 1) * 3
     return score
 
 
-def find_tool(opponent: str, outcome:str):
-    if outcome == DRAW:
+def find_tool(opponent: Tool, outcome: Outcome):
+    if outcome == Outcome.DRAW:
         return opponent
-    if outcome == LOSE:
+    if outcome == Outcome.LOSE:
         return nemesis_map[nemesis_map[opponent]]
     else:
         return nemesis_map[opponent]
 
 
-def play2(opponent: str, outcome:str):
+def play2(opponent: Tool, outcome: Outcome):
     my_tool = find_tool(opponent, outcome)
-    score = tool_score(my_tool) + (battle(opponent, my_tool)+1) * 3
+    score = tool_score(my_tool) + (battle(opponent, my_tool) + 1) * 3
     return score
 
 
 scores = []
 scores2 = []
 for l in f.readlines():
-    scores.append(play(to_tool(l[0]), to_tool(l[2])))
-    scores2.append(play2(to_tool(l[0]), l[2]))
+    scores.append(play(Tool.from_str(l[0]), Tool.from_str(l[2])))
+    scores2.append(play2(Tool.from_str(l[0]), Outcome(l[2])))
 
 print(sum(scores))
 print(sum(scores2))
