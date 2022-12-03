d4435c977af7256883132120c8b06576f17239f6
Marco Silva <marcosilva0000@gmail.com>
2022-12-03 19:55:39 +0000

    day 2 rust cleanup fn to enums

--- 2/src/main.rs
+++ 2/src/main.rs
@@ -1,11 +1,7 @@
-use std::collections::HashMap;
 use std::env;
 use std::fs;
 
-#[derive(Debug)]
 #[derive(PartialEq)]
-#[derive(Eq)]
-#[derive(Hash)]
 enum Tool {
     Rock,
     Paper,
@@ -18,41 +14,35 @@ impl Tool {
                 "B"|"Y" => Tool::Paper,
                 _ => Tool::Scissors,
         };
- }
-}
-fn tool_score(tool: &Tool) -> i32{
-    return match tool {
-        Tool::Rock => 1,
-        Tool::Paper => 2,
-        Tool::Scissors => 3,
-    };
-}
+    }
 
-fn nemesis_map(tool: &Tool) -> Tool {
-    return match tool {
-        Tool::Rock => Tool::Paper,
-        Tool::Paper => Tool::Scissors,
-        Tool::Scissors => Tool::Rock,
+    fn score(tool: &Tool) -> i32{
+        return match tool {
+            Tool::Rock => 1,
+            Tool::Paper => 2,
+            Tool::Scissors => 3,
+        };
+    }
+    fn nemesis_map(tool: &Tool) -> Tool {
+        return match tool {
+            Tool::Rock => Tool::Paper,
+            Tool::Paper => Tool::Scissors,
+            Tool::Scissors => Tool::Rock,
+        }
+    }
+    fn battle(&self, p2: &Tool) -> i32 {
+        if self == p2 {
+            return 0;
+        } 
+        else if self == &Tool::nemesis_map(p2) {
+            return -1; 
+        }
+        else{
+            return 1;
+        }
     }
 }
 
-
-fn battle(p1: &Tool, p2: &Tool) -> i32 {
-    let nemesis_map = HashMap::from([
-        (Tool::Rock, Tool::Paper),
-        (Tool::Paper, Tool::Scissors),
-        (Tool::Scissors, Tool::Rock),
-    ]);
-    if p1 == p2 {
-        return 0;
-    } 
-    else if *p1 == nemesis_map[&p2] {
-        return -1; 
-    }
-    else{
-        return 1;
-    }
-}
 
 #[derive(Debug)]
 enum Outcome {
@@ -68,29 +58,28 @@ impl Outcome {
                 _ => Outcome::Win,
         }
     }
+    fn find_tool(&self, opponent: &Tool) -> Tool {
+        return match self  {
+            Outcome::Draw => match opponent {
+                Tool::Paper => Tool::Paper,
+                Tool::Rock => Tool::Rock,
+                Tool::Scissors => Tool::Scissors,
+            },
+            Outcome::Lose => Tool::nemesis_map(&Tool::nemesis_map(opponent)),
+            Outcome::Win => Tool::nemesis_map(opponent),
+        }
+    }
 }
 
-fn find_tool(opponent: &Tool, outcome: &Outcome) -> Tool {
-  return match outcome  {
-      Outcome::Draw => match opponent {
-        Tool::Paper => Tool::Paper,
-        Tool::Rock => Tool::Rock,
-        Tool::Scissors => Tool::Scissors,
-      },
-      Outcome::Lose => nemesis_map(&nemesis_map(opponent)),
-      Outcome::Win => nemesis_map(opponent),
-  }
-
-}
 
 fn play(opponent: &Tool, me: &Tool) -> i32 {
-    let score = tool_score(me) + (battle(opponent, me) + 1)*3;
+    let score = Tool::score(me) + (opponent.battle(me) + 1)*3;
     return score;
 }
 
 fn play2(opponent: &Tool, outcome: &Outcome) -> i32 {
-    let my_tool = find_tool(opponent, outcome);
-    let score = tool_score(&my_tool) + (battle(opponent, &my_tool) + 1)*3;
+    let my_tool = outcome.find_tool(opponent);
+    let score = Tool::score(&my_tool) + (opponent.battle(&my_tool) + 1)*3;
     return score;
 }
 
@@ -111,12 +100,7 @@ fn main() {
         let outcome = Outcome::from_str(l.chars().nth(2).unwrap().to_string().as_str());
         first.extend([play(&tool, &second_tool)]);
         second.extend([play2(&tool, &outcome)]);
-
-        println!("{:?}", tool);
-        println!("{:?}", second_tool);
-        println!("{:?}", outcome);
     }
-    println!("{:?}", first);
     let sum: i32 = first.iter().sum();
     let sum2: i32 = second.iter().sum();
     println!("{sum}");
