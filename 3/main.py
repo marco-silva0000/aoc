d04adf94cfc820b1c801bd5fc302c6c06df57174
Marco Silva <marcosilva0000@gmail.com>
2022-12-03 17:14:30 +0000

    day 3 python part 2 and refactor part 1

--- 3/main.py
+++ 3/main.py
@@ -3,30 +3,55 @@ from enum import Enum
 
 f = open("3/input.txt")
 
-print(ord('a'))
-print(ord('z'))
-print(ord('A'))
-print(ord('Z'))
-findings = []
-for l in f.readlines():
+def get_score(c: str) -> int:
+    score = ord(c)
+    if score <= 90: # uppercase
+        score += -64 + 26
+    else:
+        score -= 96
+    return score
+
+
+def find_common(first: str, second: str) -> str:
+    for c in first:
+        if c in second:
+            return c
+    raise ValueError('no common items found')
+
+def find_commons(first: str, second: str) -> str:
+    result = ""
+    for c in first:
+        if c in second:
+            if c not in result:
+                result += c
+    return result
+
+repeated_findings = []
+def calc_first(l: str):
     half = int(len(l)/2)
     first = l[:half]
     second = l[half:]
-    print(f"{first}|{second}")
-    for c in first:
-        if c in second:
-            print(c)
-            score = ord(c)
-            print(score)
-            if score <= 90: # uppercase
-                score += -64 + 26
-            else:
-                score -= 96
-            print(score)
-            findings.append(score)
-            break
-print(findings)
-print(sum(findings))
+    common = find_common(first, second)
+    repeated_findings.append(get_score(common))
+
+lines = iter(f.readlines())
+badge_findings = []
+for first_bag in lines:
+    calc_first(first_bag)
+    second_bag = next(lines)
+    calc_first(second_bag)
+    third_bag = next(lines)
+    calc_first(third_bag)
+    badge = find_commons(find_commons(first_bag, second_bag), third_bag).strip()
+    badge_findings.append(get_score(badge))
+
+
+
+
+print(repeated_findings)
+print(sum(repeated_findings))
+print(badge_findings)
+print(sum(badge_findings))
 
 
 f.close()
