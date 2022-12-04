00fec2deac09675264b1852a7f6582c11386664a
Marco Silva <marcosilva0000@gmail.com>
2022-12-04 16:49:59 +0000

    day 4 rust

--- 4/src/main.rs
+++ 4/src/main.rs
@@ -2,46 +2,21 @@ use std::env;
 use std::fs;
 
 
-fn get_score(c: char) -> i32 {
-    let mut score = c as i32;
-
-    if score <= 90 { // uppercase
-        score += -64 + 26;
-    }
-    else {
-        score -= 96;
-    }
-    println!("{:?}=>{:?}->{:?}", c as i32, c, score);
-    return score;
-}
-
-fn find_common(first: &str, second: &str) -> Option<char> {
-    println!("{:?}|{:?}", first, second);
-    for c in first.chars() {
-        if second.contains(c) {
-            return Some(c);
-        }
-    }
-    return None;
-}
-
-fn find_commons(first: &str, second: &str) -> Vec<char>{
-    let mut result: Vec<char> = Vec::new();
-    println!("{:?}|{:?}", first, second);
-    for c in first.chars() {
-        if second.contains(c) {
-            result.extend([c]);
-        }
-    }
-    println!("{:?}", result);
+fn bitshift(section: &str) -> u128 {
+    // println!("{section}");
+    let (start, finish) = section.split_once('-').unwrap();
+    let start_bin = start.parse::<u128>().unwrap();
+    let finish_bin = finish.parse::<u128>().unwrap();
+    let size = finish_bin - start_bin;
+    let mask: u128 = 2u128.pow(u32::try_from(size + 1).unwrap()) -1;
+    let result = mask << start_bin;
+    // println!("{:0>128b}", start_bin);
+    // println!("{:0>128b}", finish_bin);
+    // println!("{:0>128b}", mask);
+    // println!("{:0>128b}", result);
     return result;
 }
 
-fn calc_first(line: &str) -> i32 {
-    let half = line.len()/2;
-    let common = find_common(&line[..half], &line[half..]).unwrap();
-    return get_score(common);
-}
 
 fn main() {
     let mut current_dir = env::current_dir().unwrap().to_str().unwrap().to_owned();
@@ -49,33 +24,23 @@ fn main() {
     if !current_dir.ends_with(today) {
         current_dir += today
     }
-    let contents = fs::read_to_string("3/input.txt").expect("couldn't read file");
-    let mut first: Vec<i32> = vec![];
-    let mut second: Vec<i32> = vec![];
-    // let mut second: Vec<i32> = vec![];
-    let mut lines = contents.lines();
-    loop {
-        let first_bag_option = lines.next();
-        let first_bag = match first_bag_option {
-            Some(bag) => bag,
-            None => break,
-        };
-        let second_bag = lines.next().unwrap();
-        let third_bag = lines.next().unwrap();
-        first.extend([calc_first(&first_bag)]);
-        first.extend([calc_first(&second_bag)]);
-        first.extend([calc_first(&third_bag)]);
-        let temp = find_commons(first_bag, second_bag);
-        let temp_string: String = temp.into_iter().collect();
-        let temp2 = find_commons(&temp_string, third_bag);
-        let badge: String = temp2.clone().into_iter().collect();
-        second.extend([get_score(badge.chars().next().unwrap())]);
+    let contents = fs::read_to_string("4/input.txt").expect("couldn't read file");
+    let mut result = 0;
+    let mut result2 = 0;
+    let lines = contents.lines();
+    for l in lines.into_iter() {
+        // println!("{l}");
+        let (elf1, elf2) = l.split_once(',').unwrap();
+        let elf1_bit = bitshift(elf1);
+        let elf2_bit = bitshift(elf2);
+        let or_result = elf1_bit | elf2_bit;
+        if (elf1_bit == or_result) || (elf2_bit == or_result) {
+            result += 1;
+        }
+        if (elf1_bit & elf2_bit) != 0u128 {
+            result2 += 1;
+        }
     }
-
-    let sum: i32 = first.iter().sum();
-    let sum2: i32 = second.iter().sum();
-    println!("{:?}", first);
-    println!("{sum}");
-    println!("{:?}", second);
-    println!("{sum2}");
+    println!("{result}");
+    println!("{result2}");
 }
