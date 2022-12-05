938c5d0066f25b284df4a443cae0b71426a46412
msilva <msilva@synopsys.com>
2022-12-05 20:41:01 +0000

    day 5 rust

--- 5/src/main.rs
+++ 5/src/main.rs
@@ -1,46 +1,87 @@
+use std::collections::HashMap;
 use std::env;
 use std::fs;
 
-
-fn bitshift(section: &str) -> u128 {
-    // println!("{section}");
-    let (start, finish) = section.split_once('-').unwrap();
-    let start_bin = start.parse::<u128>().unwrap();
-    let finish_bin = finish.parse::<u128>().unwrap();
-    let size = finish_bin - start_bin;
-    let mask: u128 = 2u128.pow(u32::try_from(size + 1).unwrap()) -1;
-    let result = mask << start_bin;
-    // println!("{:0>128b}", start_bin);
-    // println!("{:0>128b}", finish_bin);
-    // println!("{:0>128b}", mask);
-    // println!("{:0>128b}", result);
-    return result;
+fn print_crates(crates: &HashMap<u32, Vec<char>>) {
+    for i in 1..crates.keys().len() + 1 {
+        print!("{i}: ");
+        let row = crates.get(&(i as u32)).unwrap();
+        for j in 0..row.len() {
+            let item = row.get(j).unwrap();
+            print!("{item}");
+        }
+        print!("\n");
+    }
 }
-
 
 fn main() {
     let mut current_dir = env::current_dir().unwrap().to_str().unwrap().to_owned();
-    let today = "/4";
+    let today = "/5";
     if !current_dir.ends_with(today) {
         current_dir += today
     }
-    let contents = fs::read_to_string("4/input.txt").expect("couldn't read file");
-    let mut result = 0;
-    let mut result2 = 0;
+    let contents = fs::read_to_string("5/input.txt").expect("couldn't read file");
     let lines = contents.lines();
+    let mut crates: HashMap<u32, Vec<char>> = HashMap::new();
+    let mut crates2: HashMap<u32, Vec<char>> = HashMap::new();
     for l in lines.into_iter() {
-        // println!("{l}");
-        let (elf1, elf2) = l.split_once(',').unwrap();
-        let elf1_bit = bitshift(elf1);
-        let elf2_bit = bitshift(elf2);
-        let or_result = elf1_bit | elf2_bit;
-        if (elf1_bit == or_result) || (elf2_bit == or_result) {
-            result += 1;
-        }
-        if (elf1_bit & elf2_bit) != 0u128 {
-            result2 += 1;
+        if l.contains("[") {
+            for c in (0..l.len()).step_by(4) {
+                println!("{:?}", c);
+                if !l.chars().nth(c + 1).unwrap().is_whitespace() {
+                    let index = (c / 4 + 1) as u32;
+                    let item = l.chars().nth(c + 1).unwrap();
+                    println!("{:?}", index);
+                    println!("{:?}", item);
+                    if !crates.contains_key(&index) {
+                        crates.insert(index, vec![item]);
+                        crates2.insert(index, vec![item]);
+                    } else {
+                        crates.get_mut(&index).unwrap().extend(vec![item]);
+                        crates2.get_mut(&index).unwrap().extend(vec![item]);
+                    }
+                }
+            }
+        } else if l.starts_with("move") {
+            print_crates(&crates2);
+            println!("{:?}", l);
+            let (ammount, from_to) = l
+                .strip_prefix("move ")
+                .unwrap()
+                .split_once(" from ")
+                .unwrap();
+            let (origin, destiny) = from_to.split_once(" to ").unwrap();
+            let ammount = ammount.parse::<u32>().unwrap();
+            let origin = origin.parse::<u32>().unwrap();
+            let destiny = destiny.parse::<u32>().unwrap();
+
+            // Part1
+            for _ in 0..ammount {
+                let item = crates.get_mut(&origin).unwrap().remove(0);
+                crates.get_mut(&destiny).unwrap().insert(0, item);
+            }
+            // Part2
+            let (chunk, remainder) = crates2.get_mut(&origin).unwrap().split_at(ammount as usize);
+            let mut chunk_vec: Vec<char> = chunk.to_vec();
+            let remainder_vec: Vec<char> = remainder.to_vec();
+            // println!("chunk {:?}", chunk_vec);
+            let destiny_item = crates2.get_mut(&destiny).unwrap();
+            // println!("destiny_item {:?}", destiny_item);
+            chunk_vec.extend_from_slice(destiny_item);
+            crates2.remove(&destiny);
+            crates2.insert(destiny, chunk_vec);
+            crates2.remove(&origin);
+            crates2.insert(origin, remainder_vec);
         }
     }
-    println!("{result}");
-    println!("{result2}");
+    for i in 1..crates.keys().len() + 1 {
+        let item = crates.get(&(i as u32)).unwrap().get(0).unwrap();
+        print!("{item}");
+    }
+    print!("\n");
+    for i in 1..crates2.keys().len() + 1 {
+        let item = crates2.get(&(i as u32)).unwrap().get(0).unwrap();
+        print!("{item}");
+    }
+    print!("\n");
 }
