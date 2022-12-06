92b4c38dedc3f33cbf6472e98ba031afc08eb98a
Marco Silva <marcosilva0000@gmail.com>
2022-12-06 23:53:44 +0000

    day 6 rust refactor

--- 6/src/main.rs
+++ 6/src/main.rs
@@ -2,6 +2,24 @@ use std::collections::HashSet;
 use std::env;
 use std::fs;
 
+fn is_unique(part: &[char]) -> bool {
+    let mut uniques = HashSet::new();
+    part.iter().for_each(|c: &char| {
+        uniques.insert(c);
+    });
+    return uniques.len() == part.len();
+}
+
+fn find_non_repeating_sequence(input: &str, window_size: usize) -> Option<(usize, String)> {
+    for (i, part) in input.chars().collect::<Vec<char>>().windows(window_size).enumerate() {
+        if is_unique(part) {
+            return Some((i + window_size, part.into_iter().collect::<String>()));
+        }
+    }
+   return None;
+}
+
+
 fn main() {
     let mut current_dir = env::current_dir().unwrap().to_str().unwrap().to_owned();
     let today = "/6";
@@ -10,36 +28,11 @@ fn main() {
     }
     let contents = fs::read_to_string("6/input.txt").expect("couldn't read file");
     let input = contents.as_str();
+    let (com_start, com_string) = find_non_repeating_sequence(input, 4).unwrap();
+    let (message_start, message_string) = find_non_repeating_sequence(&input[com_start..], 14).unwrap();
     println!("{:?}", input);
-    let inter = input.chars().collect::<Vec<char>>();
-    let window_size = 4;
-    let window_size2 = 14;
-    let mut packet_start = 0;
-    let mut windows = inter.windows(window_size);
-    for i in 0..input.len() {
-        let part = windows.next().unwrap();
-        let mut uniques = HashSet::new();
-        part.iter().for_each(|c: &char| {
-            uniques.insert(c);
-        });
-        if uniques.len() == window_size {
-            packet_start = i + window_size;
-            println!("{:?}", part);
-            println!("{:?}", packet_start);
-            break;
-        }
-    }
-    let mut windows2 = inter[packet_start..].windows(window_size2);
-    for i in 0..input.len() {
-        let part = windows2.next().unwrap();
-        let mut uniques = HashSet::new();
-        part.iter().for_each(|c: &char| {
-            uniques.insert(c);
-        });
-        if uniques.len() == window_size2 {
-            println!("{:?}", part);
-            println!("{:?}", packet_start + i + window_size2);
-            break;
-        }
-    }
+    println!("{:?}", com_string);
+    println!("{:?}", com_start);
+    println!("{:?}", message_string);
+    println!("{:?}", message_start + com_start);
 }
