aee0cea17a451e8affbc6724b31b18d1675d38b4
msilva <msilva@synopsys.com>
2023-12-01 16:52:31 +0000

    add ints to matching_dict

--- aoc23/1/main.py
+++ aoc23/1/main.py
@@ -1,6 +1,9 @@
 numbers_dict = {key: index for index, key in enumerate("one two three four five six seven eight nine".split(" "), start=1) }
+digits_dict = {str(key): index for index, key in enumerate(range(1, 10), start=1) }
+numbers_dict.update(digits_dict)
 reverse_numbers_dict = {key[::-1]: value for key, value in numbers_dict.items()}
 
+f = open("1/test.txt")
 f = open("1/input.txt")
 
 
@@ -19,23 +22,22 @@ def part2_process_line(l, matching_dict=numbers_dict):
     for c in l:
         # print("c", c)
         # print("current_word", current_word)
-        if c.isdigit():
-            first_digit = int(c)
+        current_word += c
+        current_word = current_word[-5:]
+        if c in matching_dict:
+            first_digit = matching_dict[c]
             break
-        else:
-            current_word += c
-            current_word = current_word[-5:]
-            if current_word in dict_keys:
-                first_digit = matching_dict[current_word]
-                break
-            elif (substring := current_word[1:]) in dict_keys:
-                # print("substring", substring)
-                first_digit = matching_dict[substring]
-                break
-            elif (substring := current_word[2:]) in dict_keys:
-                # print("substring", substring)
-                first_digit = matching_dict[substring]
-                break
+        elif current_word in dict_keys:
+            first_digit = matching_dict[current_word]
+            break
+        elif (substring := current_word[1:]) in dict_keys:
+            # print("substring", substring)
+            first_digit = matching_dict[substring]
+            break
+        elif (substring := current_word[2:]) in dict_keys:
+            # print("substring", substring)
+            first_digit = matching_dict[substring]
+            break
     return first_digit
 
 
@@ -46,7 +48,7 @@ for l in f.readlines():
     print(l)
     first = get_first_non_error_result(l, lambda x: int(x))
     last = get_first_non_error_result(l[::-1], lambda x: int(x))
-    part1_result.append(int(f"{first}{last}"))
+    # part1_result.append(int(f"{first}{last}"))
     first_part_2 = part2_process_line(l)
     last_part_2 = part2_process_line(l[::-1], matching_dict=reverse_numbers_dict)
     part2_result.append(int(f"{first_part_2}{last_part_2}"))
