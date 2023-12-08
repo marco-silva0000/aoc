cb9b511cca92bd5b81e077a86de50813e55ad7db
Marco Silva <marco-silva0000@users.noreply.github.com>
2023-12-08 17:20:54 +0000

    day 7 rust wrong, day 8 rust

--- aoc23/7/src/main.rs
+++ aoc23/7/src/main.rs
@@ -3,61 +3,97 @@ use std::collections::HashMap;
 use std::collections::HashSet;
 use std::env;
 use std::fs;
+use std::ops::Index;
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord)]
+enum HandRank {
+    HighCard = 1,
+    OnePair = 2,
+    TwoPairs = 3,
+    ThreeOfAKind = 4,
+    FullHouse = 5,
+    FourOfAKind = 6,
+    FiveOfAKind = 7,
+}
+
+#[derive(Debug)]
+struct Hand {
+    cards: Vec<char>,
+    rank: HandRank,
+    bid: usize,
+}
+
+impl Hand {
+    fn new(hand: &str, bid: usize) -> Hand {
+        use HandRank::*;
+        let counts = hand.chars().counts();
+        let values = counts.values().sorted().join("");
+        let rank = match values.as_str() {
+            "5" => FiveOfAKind,
+            "14" => FourOfAKind,
+            "23" => FullHouse,
+            "113" => ThreeOfAKind,
+            "122" => TwoPairs,
+            "1112" => OnePair,
+            "11111" => HighCard,
+            value => unreachable!("invalid hand `{}`", value),
+        };
+        Self {
+            cards: hand.chars().collect(),
+            rank,
+            bid,
+        }
+    }
+}
 
 fn main() {
     let mut current_dir = env::current_dir().unwrap().to_str().unwrap().to_owned();
-    if !current_dir.ends_with("/4") {
-        current_dir += "/4"
+    if !current_dir.ends_with("/7") {
+        current_dir += "/7"
     }
-    let contents = fs::read_to_string(current_dir + "/input.txt").expect("couldn't read file");
+    let contents = fs::read_to_string(current_dir + "/input2.txt").expect("couldn't read file");
     let lines = contents.lines();
 
     // println!("{:?}", lines);
-    let result = lines.into_iter().map(|l| {
-        // println!("{:?}", l);
-        // Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
-        l.split_once(':')
-            .unwrap()
-            .1
-            .split('|')
-            .map(|part| {
-                // println!("{:?}", part);
-                part.split_whitespace()
-                    .fold(HashSet::new(), |mut acc, val| {
-                        // println!("{:?}", val);
-                        acc.insert(val.parse::<usize>().unwrap());
-                        acc
-                    })
-            })
-            .into_iter()
-            .reduce(|acc, val| {
-                // println!("{:?} {:?}", acc, val);
-                acc.intersection(&val).cloned().collect()
-            })
-            .unwrap()
-            .iter()
-            .count()
-    });
-    let part1 = result
+    let part1_rank = "23456789TJQKA";
+    let sorted_counts = vec![(
+        (5,),
+        (4, 1),
+        (3, 2),
+        (3, 1, 1),
+        (2, 2, 1),
+        (2, 1, 1, 1),
+        (1, 1, 1, 1, 1),
+    )];
+    // line has 7KAK7 63
+
+    let hands = lines
+        .into_iter()
+        .map(|l| {
+            // println!("{:?}", l);
+            // Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+            l.split_once(' ').unwrap()
+        })
+        .map(|(hand, bid)| (hand, bid.parse::<usize>().unwrap()));
+    let part1 = hands
         .clone()
-        .filter(|x| *x > 0)
-        .map(|x| 2_i32.pow(x as u32 - 1))
-        .sum::<i32>();
-    println!("{:?}", part1);
-    let mut part2_result: HashMap<usize, usize> = HashMap::new();
-
-    result.clone().enumerate().for_each(|(i, x)| {
-        // println!("Card {:?} has {:?} matching numbers, so you win one copy each of the next {:?} cards: cards {:?}", i+1, x, x, i + 2..i + 2 + x);
-        *part2_result.entry(i + 1).or_insert(0) += 1;
-        if x > 0 {
-            let value_to_insert = part2_result.entry(i + 1).or_insert(1).to_owned();
-            for j in i + 1..i + 1 + x {
-                // println!("inserting {:?} on {:?}",value_to_insert, j + 1);
-                *part2_result.entry(j + 1).or_insert(0) += value_to_insert;
+        .map(|(hand, bid)| Hand::new(hand, bid))
+        .sorted_by(|a, b| {
+            let a_rank = a.rank as usize;
+            let b_rank = b.rank as usize;
+            if a_rank == b_rank {
+                todo!();
+                // return a.cards.iter().cmp(&b.bid);
             }
-        }
-    });
-    println!("{:?}", result.clone().collect::<Vec<usize>>());
+            a_rank.cmp(&b_rank)
+        });
 
-    println!("{:?}", part2_result.values().sum::<usize>());
+    // .sorted_by(|a, b| {
+    //     println!("a {:?}", a.0);
+    //     todo!();
+    //     // sorted_counts
+    //     //     .index::<usize>(a.0)
+    //     //     .cmp(&sorted_counts.index(b.0))
+    // });
+    println!("part1: {:?}", part1);
 }
