896ce67c3db379f0578b62f8db93af320dcfb9e9
Marco Silva <marco-silva0000@users.noreply.github.com>
2024-12-16 08:23:47 +0000

    add day 16 python

---
+++ aoc24/day-16/src/part1.rs
@@ -0,0 +1,4 @@
+// cargo run -r -p aoc24_16 --bin part1
+pub fn process(_input: &str) -> miette::Result<String> {
+    todo!("day 16 - part 1");
+}
---
+++ aoc24/day-16/src/part2.rs
@@ -0,0 +1,4 @@
+// cargo run -r -p aoc24_16 --bin part2
+pub fn process(_input: &str) -> miette::Result<String> {
+    todo!("day 16 - part 2");
+}
