ca2a57b02fb58bdf94d232e2c218b1bd38045a4d
Marco Silva <marco-silva0000@users.noreply.github.com>
2024-12-19 07:23:39 +0000

    add day 18 python

---
+++ aoc24/day-18/main.py
@@ -0,0 +1,104 @@
+import os
+from structlog import get_logger
+import pytest
+
+log = get_logger()
+
+from .part1 import part1
+from .part2 import part2
+
+
+def parse(data: str):
+    # current_dir = os.path.dirname(os.path.abspath(__file__))
+    lines = data.splitlines()
+    values_list = []
+    for line in lines:
+        values_list.append(line)
+    return values_list
+
+
+def get_data():
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    f = open(f"{current_dir}/input.txt")
+    data = f.read()
+    f.close()
+    return data
+
+
+if __name__ == "__main__":
+    data = get_data()
+    parsed_data = parse(data)
+    print(part1(parsed_data))
+    print(part2(parsed_data))
+
+
+def run_part1(parsed_data):
+    print(part1(parsed_data))
+
+
+def run_part2(parsed_data):
+    print(part2(parsed_data))
+
+
+def test_part1():
+    data = """5,4
+4,2
+4,5
+3,0
+2,1
+6,3
+2,4
+1,5
+0,6
+3,3
+2,6
+5,1
+1,2
+5,5
+2,5
+6,5
+1,4
+0,4
+6,4
+1,1
+6,1
+1,0
+0,5
+1,6
+2,0"""
+    parsed_data = parse(data)
+    log.debug(parsed_data)
+    result = part1(parsed_data)
+    assert result == "22"
+
+
+def test_part2():
+    data = """5,4
+4,2
+4,5
+3,0
+2,1
+6,3
+2,4
+1,5
+0,6
+3,3
+2,6
+5,1
+1,2
+5,5
+2,5
+6,5
+1,4
+0,4
+6,4
+1,1
+6,1
+1,0
+0,5
+1,6
+2,0"""
+    parsed_data = parse(data)
+    log.debug(parsed_data)
+    result = part2(parsed_data)
+    assert result == "6,1"
---
+++ aoc24/day-18/src/part2.rs
@@ -0,0 +1,4 @@
+// cargo run -r -p aoc24_18 --bin part2
+pub fn process(_input: &str) -> miette::Result<String> {
+    todo!("day 18 - part 2");
+}
