1d74dac67a0cb547ab27c8d0071ccf80835aaa20
Marco Silva <marco-silva0000@users.noreply.github.com>
2025-12-02 16:39:41 +0200

    improve day 25 part 2 with caching and early return

--- aoc25/day-2/part2.py
+++ aoc25/day-2/part2.py
@@ -6,6 +6,8 @@ import structlog
 from dataclasses import dataclass
 from itertools import cycle
 from more_itertools import grouper
+from sympy.ntheory import primefactors
+from functools import cache
 
 logger = structlog.get_logger()
 
@@ -20,6 +22,49 @@ def is_invalid(id_to_validate: int) -> bool:
         logger.debug(f"set_to_validate: {set_to_validate}")
         if len(set_to_validate) == 1:
             return True
+    return False
+
+
+@cache
+def get_factors(length):
+    result = []
+    for i in range(1, length // 2 + 1):
+        if length % i == 0:
+            result.append(i)
+    return result
+
+
+def is_invalid(id_to_validate: int) -> bool:
+    str_id_to_validate = str(id_to_validate)
+    logger.debug(f"validating: {str_id_to_validate}")
+    len_str_id_to_validate = len(str_id_to_validate)
+    factors = get_factors(len_str_id_to_validate)
+    logger.debug(f"factors: {factors}")
+    for factor in factors:
+        set_to_validate = set(grouper(str_id_to_validate, factor, fillvalue="x"))
+        logger.debug(f"factor:{factor} set_to_validate: {set_to_validate}")
+        if len(set_to_validate) == 1:
+            return True
+    return False
+
+
+def is_invalid(id_to_validate: int) -> bool:
+    str_id_to_validate = str(id_to_validate)
+    logger.debug(f"validating: {str_id_to_validate}")
+    len_str_id_to_validate = len(str_id_to_validate)
+    factors = get_factors(len_str_id_to_validate)
+    logger.debug(f"factors: {factors}")
+    for factor in factors:
+        group_to_validate = grouper(str_id_to_validate, factor, fillvalue="x")
+        prev = next(group_to_validate)
+        for value in group_to_validate:
+            if prev != value:
+                break
+        else:
+            return True
+        # logger.debug(f"factor:{factor} set_to_validate: {set_to_validate}")
+        # if len(set_to_validate) == 1:
+        # return True
     return False
 
 
